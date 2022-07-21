607aa5562f6a4993aa3783685eee9a84260c67ad
Akshat Jain <akshat.jn2002@gmail.com>
2022-07-21 16:55:34 +0530

    done

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      It's my pleasure to introduce my self..well, I'm parsuing my B-Tech in stream of Electronics and Communication engineering with specialization in internet of things from
+      It's my pleasure to introduce my self..well, I'm pursuing my B-Tech in stream of Electronics and Communication engineering with specialization in internet of things from
                 NSUT. I'm a district level Table Tennis Player.
                 My strengths are my attitude that i like to take challenges that I CAN do it,my way of thinking that i 
                 take both success and failure in a balanced manner..
